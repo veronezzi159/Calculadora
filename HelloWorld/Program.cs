c4a0cbc10bd10e749745b7cde721b37754e0bb79
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 01:22:39 -0300

    Commit com as alterações necessarias para escolher o tipo de operação

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -17,32 +17,70 @@ Console.WriteLine("\nVocê tem: " + idade + " anos");
 */
 
 float n1, n2;
-
+string operador;
+bool controle;
 float soma;
 float sub;
 float multipli;
 float div;
 
-Console.WriteLine("Digite 2 numeros( separando-os por linha): ");
-n1 = float.Parse(Console.ReadLine());
-n2 = float.Parse(Console.ReadLine());
 
-soma  = n1 + n2 ;
-sub = n1 - n2 ;
-multipli = n1 * n2;
- if ((n2 == 0) || (n2 < 0) )
+do
 {
-    Console.WriteLine("Impossível Dividir por zero ou negativo");
-} else
+    Console.WriteLine("Qual operação deseja realiar? use: s para soma, sub para subtração, m para multiplicação e d para divisão");
+    operador = Console.ReadLine();
+    if (operador == "s" || operador == "d" || operador == "sub" || operador == "m")
+    {
+        controle = true;
+    }
+    else
+    {
+        controle= false;
+    }
+
+
+} while (controle == false);
+
+
+do 
+{
+    Console.WriteLine("Digite 2 numeros( separando-os por linha): ");
+    n1 = float.Parse(Console.ReadLine());
+    n2 = float.Parse(Console.ReadLine());
+    if (operador == "d")
+    {
+        if (n2 > 0)
+        {
+            controle = true;
+        }
+        else
+        {
+            Console.WriteLine("Divisão por zero/negativo impossivel, digite um numero valido");
+            controle = false;
+        }
+    }
+    else
+    {
+        controle = true;
+    }
+} while (controle == false);
+
+if (operador == "s")
+{
+    soma = n1 + n2;
+    Console.WriteLine($"Soma: {soma}");
+} else if (operador == "d")
 {
     div = n1 / n2;
     Console.WriteLine($"Divisão: {div}");
+} else if (operador == "m")
+{
+    multipli = n1 * n2;
+    Console.WriteLine($"Multiplicação: {multipli}");
+} else
+{
+    sub = n1 - n2;
+    Console.WriteLine($"Subtração: {sub}");
 }
-
-Console.WriteLine($"Soma: {soma}");
-Console.WriteLine($"Subtração: {sub}");
-Console.WriteLine($"Multiplicação: {multipli}");
-
-
 
 Console.ReadLine();
