a755774619fc0e0a9a34d3268f5a38859918cb43
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-19 14:41:52 -0300

    Correção de erros

---
+++ Calculadora1/Program.cs
@@ -0,0 +1,49 @@
+﻿float n1, n2;
+int op = 0;
+float resultado;
+
+
+Console.WriteLine("Digite 1 numero: ");
+n1 = float.Parse(Console.ReadLine());
+do
+{
+    Console.WriteLine( "Digite outro numero (diferente de zero)");
+    n2 = float.Parse(Console.ReadLine());
+} while (n2 == 0);
+
+do
+{
+    Console.WriteLine("Escolha uma operação");
+    Console.Write("1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 - Divisão");
+    op = int.Parse(Console.ReadLine());
+} while (op < 1 && op > 4 );
+
+if (op == 1)
+{
+    resultado = n1 + n2;
+    Console.WriteLine(resultado);
+}
+else
+{
+    if (op == 2)
+    {
+        resultado = n1 - n2;
+        Console.WriteLine( resultado);
+    }
+    else
+    {
+        if (op == 3)
+        {
+            resultado = n1 * n2;
+            Console.WriteLine( resultado);
+        }
+        else
+        {
+            resultado = n1 / n2;
+            Console.WriteLine( resultado);
+        }
+    }
+}
+
+
+Console.ReadLine();
